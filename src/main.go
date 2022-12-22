9b0daaecd02a59624b2dbaf02a06736df52f0ec3
Vanessa Marchetti <vanessa.marchetti@sumup.com>
2022-12-22 01:58:00 -0300

    Refactor endpoints

---
+++ src/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo-contrib/prometheus"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	var path = "/"
+	// Setup Main Server
+	echoMainServer := echo.New()
+	echoMainServer.HideBanner = true
+	echoMainServer.Use(middleware.Logger())
+	echoMainServer.GET(path, hello)
+
+	// Create Prometheus server and Middleware
+	echoPrometheus := echo.New()
+	echoPrometheus.HideBanner = true
+	prom := prometheus.NewPrometheus("echo", nil)
+
+	// Scrape metrics from Main Server
+	echoMainServer.Use(prom.HandlerFunc)
+	// Setup metrics endpoint at another server
+	prom.SetMetricsPath(echoPrometheus)
+
+	go func() { echoPrometheus.Logger.Fatal(echoPrometheus.Start(":9360")) }()
+
+	echoMainServer.Logger.Fatal(echoMainServer.Start(":7090"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}

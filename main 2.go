6c45f179ae0a778fb1f1ad3b01a608a2e0ff9770
Vanessa Marchetti <vanessa.marchetti@sumup.com>
2022-12-22 15:30:13 -0300

    Another random comment

---
+++ main 2.go
@@ -0,0 +1,14 @@
+package main
+
+import "net/http"
+
+func main() {
+	var path = "/"
+	// Setup Main Server
+	echoMainServer := echo.New()
+	echoMainServer.GET(path, hello)
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}

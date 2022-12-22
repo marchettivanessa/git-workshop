a72e042d509f5a2a4c4e81d13f0b0f9bca512acb
Vanessa Marchetti <67073331+marchettivanessa@users.noreply.github.com>
2022-12-22 13:31:27 -0300

    Update main.go

--- src/main.go
+++ src/main.go
@@ -26,6 +26,7 @@ func main() {
 	// Setup metrics endpoint at another server
 	prom.SetMetricsPath(echoPrometheus)
 
+	//Random comment here
 	go func() { echoPrometheus.Logger.Fatal(echoPrometheus.Start(":9360")) }()
 
 	echoMainServer.Logger.Fatal(echoMainServer.Start(":7090"))

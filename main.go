429b465770676162ae8cb2e94d33a4898591dfb3
Vanessa Marchetti <vanessa.marchetti@sumup.com>
2022-12-22 01:58:44 -0300

    Another commit after rebase

--- main.go
+++ main.go
@@ -3,15 +3,32 @@ package main
 import (
 	"net/http"
 
-	"github.com/labstack/echo"
+	"github.com/labstack/echo-contrib/prometheus"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	var path = "/"
 	// Setup Main Server
 	echoMainServer := echo.New()
+	echoMainServer.HideBanner = true
+	echoMainServer.Use(middleware.Logger())
 	echoMainServer.GET(path, hello)
-	//New random comment
+
+	// Create Prometheus server and Middleware
+	echoPrometheus := echo.New()
+	echoPrometheus.HideBanner = true
+	prom := prometheus.NewPrometheus("echo", nil)
+
+	// Scrape metrics from Main Server
+	echoMainServer.Use(prom.HandlerFunc)
+	// Setup metrics endpoint at another server
+	prom.SetMetricsPath(echoPrometheus)
+
+	go func() { echoPrometheus.Logger.Fatal(echoPrometheus.Start(":9360")) }()
+
+	echoMainServer.Logger.Fatal(echoMainServer.Start(":7090"))
 }
 
 func hello(c echo.Context) error {
